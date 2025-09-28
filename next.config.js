bf390c3b3477fe2d8f6a4dd783fd23910569fbe1
chitransh <148608225+cursedxo1@users.noreply.github.com>
2025-09-29 04:57:41 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  env: {
+    CUSTOM_KEY: 'eco-vibe-app',
+  },
+};
+
+module.exports = nextConfig;
